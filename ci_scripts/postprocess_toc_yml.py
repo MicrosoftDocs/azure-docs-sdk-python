6f7cb405478c3772b877df71ee6d12f37ee2b613
Yannan Wang <15991684027@163.com>
2017-07-18 16:08:50 +0800

    Update postprocess_toc_yml.py

--- ci_scripts/postprocess_toc_yml.py
+++ ci_scripts/postprocess_toc_yml.py
@@ -3,6 +3,17 @@
 import io
 import yaml
 
+skipped_level2_packages = [
+    'azure.mgmt.resource.features',
+    'azure.mgmt.resource.links',
+    'azure.mgmt.resource.locks',
+    'azure.mgmt.resource.managedapplications',
+	'azure.mgmt.resource.policy',
+    'azure.mgmt.resource.resources',
+    'azure.mgmt.resource.subscriptions',
+    'azure.mgmt.compute.containerservice',
+    'azure.mgmt.compute.compute'
+]
 
 def rewrite_yml(data):
     with io.open('.\\_build\\docfx_yaml\\toc.yml', 'w', encoding='utf8') as outfile:
@@ -13,11 +24,10 @@ with open(".\\_build\\docfx_yaml\\toc.yml", 'r') as stream:
         data_loaded = yaml.load(stream)
         for node in data_loaded:
             if 'name' in node:
-                if node['name'].startswith('azure.'):
+                if node['name'].startswith('azure.') and node['name'] not in skipped_level2_packages:
                     node['name'] = node['name'].replace('.', '-')
                     print('update old name to %s' % node['name'])
 
         rewrite_yml(data_loaded)
     except yaml.YAMLError as exc:
         print(exc)
-
