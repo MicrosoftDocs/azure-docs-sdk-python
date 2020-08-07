b402c8a00f3a4eca7123d1fc33dae43f927da1fd
Scott Beddall <scbedd@microsoft.com>
2020-08-07 12:56:42 -0700

    copying folders as well now

--- update_toc.py
+++ update_toc.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
     migrating_namespaces = json["migrating_namespaces"]
 
   for config_item in migrating_namespaces:
-    target_toc_loc = os.path.join(root_dir, list(config_item.keys())[0],"toc.yml")
+    target_folder = os.path.join(root_dir, list(config_item.keys())[0])
+    target_toc_loc = os.path.join(target_folder, "toc.yml")
     source_toc_loc = os.path.join(root_dir, LEGACY_SOURCE_FOLDER, "docs-ref-autogen", "toc.yml")
     selected_targets = config_item[list(config_item.keys())[0]]
 
@@ -28,22 +29,17 @@ if __name__ == "__main__":
     with open(source_toc_loc, "r", encoding="utf-8") as source_yml:
       source_yml = yaml.safe_load(source_yml)
 
-    # with open(target_toc_loc, "r", encoding="utf-8") as target_yml:
-    #   target_yml = yaml.safe_load(target_yml)
-
     if not selected_targets:
       continue
 
-    # import pdb
-    # pdb.set_trace()
-
     toc_items = []
-    files_for_move = []
+    folders_for_move = []
 
     # filter that toc
     for index, top_level_toc_item in enumerate(source_yml):
       if top_level_toc_item["name"] in selected_targets:
         toc_items.append(top_level_toc_item)
+        folders_for_move.append(os.path.join(root_dir, LEGACY_SOURCE_FOLDER, "docs-ref-autogen", top_level_toc_item["name"]))
 
     appended_content = yaml.dump(toc_items, default_flow_style=False)
 
@@ -51,5 +47,5 @@ if __name__ == "__main__":
     with open(target_toc_loc, "a", encoding="utf-8") as stable_toc:
       stable_toc.write(appended_content)
 
-  # for file_name in files_for_move:
-  #   shutil.copy(file_name, os.path.join(root_dir, TARGET_SOURCE_FOLDER))
+  for folder in files_for_move:
+    shutil.copy(folder, os.path.join(root_dir, TARGET_SOURCE_FOLDER))
