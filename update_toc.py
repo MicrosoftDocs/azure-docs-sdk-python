05e375aaaadacfae0e16b2729bfd554639f3c81c
Scott Beddall <scbedd@microsoft.com>
2020-08-07 13:24:29 -0700

    copy to appropriate target folder

--- update_toc.py
+++ update_toc.py
@@ -50,5 +50,5 @@ if __name__ == "__main__":
       stable_toc.write(appended_content)
 
     for folder in folders_for_move:
-      print("copying {} to {}".format(folder, target_folder))
-      copy_tree(folder, target_folder)
+      print("copying {} to {}".format(folder, os.path.join(target_folder, os.path.basename(folder)))
+      copy_tree(folder, os.path.join(target_folder, os.path.basename(folder)))
